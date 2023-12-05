8a70362e0abebe919e288cd4f698836806490320
hyemin33 <joliie@naver.com>
2023-12-05 18:29:38 +0900

    add chapter11-1

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,38 @@
-export default function Page() {
-  return <p>Invoices Page</p>;
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  // 페이지 이동 시 검색어 쿼리와 현재 페이지를 가져온다. 쿼리와 페이지를 Table로 전달한다.
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="검색 중..." />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
 }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,16 +1,46 @@
 'use client';
-
+ 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-
+import { useSearchParams, usePathname, useRouter  } from 'next/navigation';
+ 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  // step4 pathname, replace 추가
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  
+  function handleSearch(term: string) {
+    // step 1
+    // console.log(term, '검색이 되는건가본가나다라');
+    
+    // step 2 
+    // useSearchParams 추가, searchParams 만들어서, 아래 params에 담기
+    const params = new URLSearchParams(searchParams);
+
+    // step 3
+    if (term) { // 검색어 있으면
+      params.set('query', term); // 검색어로 params 변경 -> .set 노션체크!
+    } else { // 아니면
+      params.delete('query'); // 삭제
+    }
+
+    // step 5 pathname으로 바꾸기 
+    replace(`${pathname}?${params.toString()}`);
+
+  }
+ 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
-        Search
+        검색
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
