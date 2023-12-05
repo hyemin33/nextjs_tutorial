d2a017e01153268ec981dcbec74f4998bc83d42d
hyemin33 <joliie@naver.com>
2023-12-05 18:33:58 +0900

    fix chapter11 : add use-debounce

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,5 +1,6 @@
 'use client';
- 
+
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter  } from 'next/navigation';
  
@@ -9,7 +10,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
   
-  function handleSearch(term: string) {
+  // step 6 handleSearch를 래핑하여 특정시간 뒤에 코드가 실행되도록 합니다.
+  const handleSearch = useDebouncedCallback((term) => {
     // step 1
     // console.log(term, '검색이 되는건가본가나다라');
     
@@ -27,7 +29,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     // step 5 pathname으로 바꾸기 
     replace(`${pathname}?${params.toString()}`);
 
-  }
+  }, 300);
  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
