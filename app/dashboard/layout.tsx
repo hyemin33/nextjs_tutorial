4be4b5b5c9ba397fa967f5fe0410a63880d51e95
hyemin33 <joliie@naver.com>
2023-12-19 17:20:01 +0900

    add chapter16

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,15 @@
+import { Metadata } from 'next';
+
+// 제목, 설명을 추가합니다. 여기에 추가하면 모든 페이지에 해당 내용이 상속됩니다.
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'next tutorial',
+  },
+  description: 'The official Next.js ',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 import SideNav from '@/app/ui/dashboard/sidenav';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
